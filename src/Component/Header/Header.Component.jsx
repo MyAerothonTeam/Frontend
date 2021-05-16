3990db49e09a327f2336cb7bf6db16d979674935
vishal <vishalshingate26@gmail.com>
2021-05-16 15:27:50 +0530

    Updating header and resolving conflicts

--- src/Component/Header/Header.Component.jsx
+++ src/Component/Header/Header.Component.jsx
@@ -2,8 +2,28 @@ import './Header.styles.css'
 
 const Header = () => {
     return(
-    <div className="header">
-    </div>
+        <div className="header">
+        <div class="title">
+        <a href="#default" className="logo">Covid<br/>Dashbord</a>
+        </div>
+       <div class="sec">
+        <label  class="header_middle" for="state">Select state :</label>
+      
+      <select name="state" id="state">
+        <option value="Maharastra">Maharastra</option>
+        <option value="keral">keral</option>
+        <option value="Up">Up</option>
+        <option value="Mp">Mp</option>
+      </select>
+         
+      <a href="#death_rate" class="header_right">Death rate/ Recovery rate</a>
+        
+
+      <a class="header_prediction" href="#prediction">prediction</a>
+         
+        </div>
+         </div>    
+
     )
 } 
 
