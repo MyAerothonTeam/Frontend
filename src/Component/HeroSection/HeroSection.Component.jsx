f9924ab313b18952131f096254d6ab3822c23bcd
shivanih99 <hebbaleshivani@gmail.com>
2021-05-16 13:55:02 +0530

    Creating-Initial-Layout-Of-Hero-Section

--- src/Component/HeroSection/HeroSection.Component.jsx
+++ src/Component/HeroSection/HeroSection.Component.jsx
@@ -3,6 +3,22 @@ import './HeroSection.styles.css'
 const HeroSection = () => {
     return(
     <div className="heroSection">
+
+       <div class="container"> 
+        <div class="box1">
+                <h3>Confirmed</h3>
+                <h1>29,12,48,00</h1>
+        </div>
+        <div class="box2">
+            <h3>Active</h3>
+                <h1>20,13,48,00</h1>
+       </div>
+        <div class="box3">
+           <h3>Recovered</h3>
+          <h1>9,12,48,00</h1>
+      </div>
+        </div>
+       
     </div>
     )
 } 
