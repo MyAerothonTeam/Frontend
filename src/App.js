2169bae11018cc1478676565cb930b42f5fbff0b
SIDDESH THORAT <siddesh.thorat@rsllearning.com>
2021-05-16 17:18:47 +0530

    Routing In React

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import HeroSection from './Component/HeroSection/HeroSection.Component'
 import LeftPanel from './Component/LeftPanel/LeftPanel.Component'
 import MainContent from './Component/MainContent/MainContent.Component'
 import RightPanel from './Component/RightPanel/RightPanel.Component'
+import { BrowserRouter,Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HeroSection />
-      <div className="contentSection">
-        <LeftPanel />
-        <MainContent />
-        <RightPanel />
+    <BrowserRouter>  
+      <div className="App">
+        {/* <Link to={'/about'}><button onClick={() => console.log("about stmt")}>About</button></Link>
+        <Link to={'/contact'}><button onClick={() => console.log("console stmt")}>Contact</button></Link> */}
+        <Header />
+        <HeroSection />
+        <div className="contentSection">
+          <LeftPanel />
+          <MainContent />
+          <RightPanel />
+        </div>
       </div>
-    </div>
+    </BrowserRouter> 
   );
 }
 
--- src/Component/MainContent/MainContent.Component.jsx
+++ src/Component/MainContent/MainContent.Component.jsx
@@ -1,8 +1,15 @@
 import './MainContent.styles.css'
+import { Route, Switch } from 'react-router-dom';
+import AboutPage from '../AboutPage/AboutPage.Component';
+import ContactPage from '../ContactPage/ContactPage.Component';
 
 const MainContent = () => {
     return(
     <div className="mainContent">
+        <Switch>
+            <Route path={'/about'} component={AboutPage} />
+            <Route path={'/contact'} component={ContactPage} />
+        </Switch>
     </div>
     )
 } 
