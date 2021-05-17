235b0c9fb22d0582578413e3d28685fc42e2d93a
vishal <vishalshingate26@gmail.com>
2021-05-17 19:22:13 +0530

    Header changes and Fetching Data

--- src/Component/HeroSection/HeroSection.Component.jsx
+++ src/Component/HeroSection/HeroSection.Component.jsx
@@ -1,19 +1,18 @@
 import './HeroSection.styles.css'
 
-const HeroSection = () => {
+const HeroSection = (props) => {
     return(
     <div className="heroSection">
-
-       <div class="container"> 
-        <div class="box1">
+       <div className="container"> 
+        <div className="box1">
                 <h3>Confirmed</h3>
                 <h1>29,12,48,00</h1>
         </div>
-        <div class="box2">
+        <div className="box2">
             <h3>Active</h3>
-                <h1>20,13,48,00</h1>
+            <h1>{props.data}</h1>
        </div>
-        <div class="box3">
+        <div className="box3">
            <h3>Recovered</h3>
           <h1>9,12,48,00</h1>
       </div>
--- src/Component/MainContent/MainContent.Component.jsx
+++ src/Component/MainContent/MainContent.Component.jsx
@@ -2,13 +2,14 @@ import './MainContent.styles.css'
 import { Route, Switch } from 'react-router-dom';
 import AboutPage from '../AboutPage/AboutPage.Component';
 import ContactPage from '../ContactPage/ContactPage.Component';
-
+import RecoveryPage from '../Recovery_View/RecoveryPage.Component';
 const MainContent = () => {
     return(
     <div className="mainContent">
         <Switch>
             <Route path={'/about'} component={AboutPage} />
             <Route path={'/contact'} component={ContactPage} />
+            <Route path={'/recovery'}component={RecoveryPage}/>
         </Switch>
     </div>
     )
